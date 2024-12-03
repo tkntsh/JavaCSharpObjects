2a94859efbb3ebd4efaf04727711f886ba0c531c
Ntokozo <ntokntshangase@gmail.com>
2024-12-03 18:04:00 +0200

    Update ageCalc.cs

--- ageCalc.cs
+++ ageCalc.cs
@@ -9,12 +9,16 @@ namespace oop1
     }
     public static void calculatedAge()
     {
+      //asking user to enter current year and store value
       Console.WriteLine("Enter the current year: ");
       int currentYear = Convert.ToInt32(Console.ReadLine());
+      //asking user to enter DOB and store value
       Console.WriteLine("Enter your year of birth: ");
       int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+      //calc age
       int age = currentYear - yearOfBirth;
-      
+
+      //printing out their age
       Console.WriteLine("Your age is: " + age);
     }
   }
