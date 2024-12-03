458dafc3bb390c5e72032cb632f396101312bf9d
Ntokozo <ntokntshangase@gmail.com>
2024-12-03 22:56:42 +0200

    Update bureauDeChange1.java

--- bureauDeChange1.java
+++ bureauDeChange1.java
@@ -1,11 +1,14 @@
 public class bureauDeChange1
 {
+    //base currency
     private static final double baseCurrency = 1.0;
 
+    //conversion rates for different currencies
     private static final double randToEuro = 0.053;
     private static final double randToDollar = 0.055;
     private static final double randToPound = 0.044;
 
+    //methods converting from rands to different currecnies
     public double convertRandToEuro(double randAmount)
     {
         return randAmount * randToEuro;
@@ -19,11 +22,13 @@ public class bureauDeChange1
         return randAmount * randToPound;
     }
 
+    //main method
     public static void main(String[] args)
     {
         bureauDeChange1 BDC = new bureauDeChange1();
         double randAmount = 100.00;
 
+        //printing out different currency balances using different methods for different currencies
         System.out.println("Your current balance in Rands is: R" + randAmount);
         System.out.println("Your current balance in Pounds is: £" + BDC.convertRandToPound(randAmount));
         System.out.println("Your current balance in Dollars is: $" + BDC.convertRandToDollar(randAmount));
